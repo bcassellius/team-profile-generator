ea62763e271db09fa513645080cec41e9059244d
Bri <bhilliker@gmail.com>
2021-08-08 15:26:36 -0500

    Add Manager Tests

---
+++ __tests__/manager.test.js
@@ -0,0 +1,41 @@
+const Manager = require("../lib/Manager");
+
+test('creates an manager object', () => {
+    const manager = new Manager('Nobody');
+    expect(typeof(manager)).toBe('object');
+});
+
+test('can set values via constructor argument', () => {
+    const manager = new Manager('Nobody', 40, "nobody@special.com", "456")
+    
+    expect (manager.name).toEqual("Nobody");
+    expect (manager.id).toEqual(40);
+    expect (manager.email).toEqual("nobody@special.com");
+    expect (manager.school).toEqual("456");
+  })
+
+// test to see if my methods for getting data work correctly
+test('creates manager name', () => {
+    const manager = new Manager('Nobody', 40, "nobody@special.com", "456");
+    expect (manager.getName()).toEqual(expect.any(String))
+});
+
+test('creates manager id', () => {
+    const manager = new Manager('Nobody', 40, "nobody@special.com", "456");
+    expect (manager.getId()).toEqual(expect.any(Number))
+});
+
+test('creates manager email', () => {
+    const manager = new Manager('Nobody', 40, "nobody@special.com", "456");
+    expect (manager.getEmail()).toEqual(expect.any(String))
+});
+
+test('creates employee role', () => {
+    const manager = new Manager('Nobody', 40, "nobody@special.com", "456");
+    expect (manager.getRole()).toBe("Manager")
+})
+
+test('creates manager school', () => {
+    const manager = new Manager('Nobody', 40, "nobody@special.com", "456");
+    expect (manager.getOfficeNumber()).toEqual(expect.any(Number))
+})
